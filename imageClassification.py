4e1cf47c4ad077597963b9f89bdb73450b6cb970
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-10-08 13:04:05 +0530

    model preparation

--- imageClassification.py
+++ imageClassification.py
@@ -1,6 +1,9 @@
 import pickle 
 import numpy as np
+import tensorflow as tf
 
+from tensorflow import keras
+from keras import layers, models
 def unpickle(file:str):
     with open (file,'rb') as f:
         dict = pickle.load(f, encoding= 'bytes')
@@ -36,3 +39,19 @@ tran_labels = np.concatenate(train_labels_batches, axis =0)
 test_images, test_labels = preprocessing(test_data)
 
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# MaxPooling layers are inserted after certain Conv2D layers to reduce spatial dimensions and enhance the network's ability to capture hierarchical features.
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu', input_shape = (32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',  # For integer labels
+              metrics=['accuracy'])
