29489b29f1a279719bcab466b94d8716c38c47b2
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-10-08 12:22:51 +0530

    preprocessing

--- imageClassification.py
+++ imageClassification.py
@@ -6,8 +6,14 @@ def unpickle(file:str):
         dict = pickle.load(f, encoding= 'bytes')
     return dict
 
+def preprocessing(data_dict: dict):
+    data = np.array(data_dict[b'data'])
+    data = data.reshape(-1, 3, 32, 32).transpose(0, 2, 3 ,1)
+    data = data/255.0
+    labels = np.array(data_dict[b'labels'])
+    return data, labels
 
-train = unpickle('data_batch_1')
+train_data = unpickle('data_batch_1')
 # print(train.keys())
 
 # for i in range(6):
@@ -15,6 +21,9 @@ train = unpickle('data_batch_1')
 #     x = unpickle(file)
 #     train.append(x)
 
-test = unpickle('test_batch')
+test_data = unpickle('test_batch')
 
+train_images, train_labels = preprocessing(train_data)
+test_images, test_labels = preprocessing(test_data)
 
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
