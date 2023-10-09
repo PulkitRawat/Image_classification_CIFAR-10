290c225c4c0f8548644f64e49dda0b8a9ecc25e0
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-10-09 15:55:38 +0530

    model created and saved

--- imageClassification.py
+++ imageClassification.py
@@ -1,9 +1,10 @@
 import pickle 
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from tensorflow import keras
 from keras import layers, models
+
 def unpickle(file:str):
     with open (file,'rb') as f:
         dict = pickle.load(f, encoding= 'bytes')
@@ -38,8 +39,6 @@ train_images  = np.concatenate(train_images_batches, axis =0)
 train_labels = np.concatenate(train_labels_batches, axis =0)
 test_images, test_labels = preprocessing(test_data)
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
 # MaxPooling layers are inserted after certain Conv2D layers to reduce spatial dimensions and enhance the network's ability to capture hierarchical features.
 model = models.Sequential()
 model.add(layers.Conv2D(32,(3,3),activation='relu', input_shape = (32,32,3)))
@@ -61,8 +60,6 @@ history = model.fit(train_images, train_labels, epochs=10, validation_split= 0.2
 test_loss, test_accuracy = model.evaluate(test_images, test_labels)
 print(f"Test accuracy: {test_accuracy}")
 
-import matplotlib.pyplot as plt
-
 # # Plot training history
 # plt.plot(history.history['accuracy'], label='accuracy')
 # plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
@@ -71,3 +68,5 @@ import matplotlib.pyplot as plt
 # plt.ylim([0, 1])
 # plt.legend(loc='lower right')
 # plt.show()
+
+model.save('IC_model')
